77a858ec7f1cdfdcc86152daa6d0bad1299ad9f2
Josselin Poiret <dev@jpoiret.xyz>
2022-11-14 15:47:50 +0100

    Make lambda bindings preserve polarity annotations

--- src/full/Agda/TypeChecking/Errors.hs
+++ src/full/Agda/TypeChecking/Errors.hs
@@ -262,6 +262,7 @@ errorString err = case err of
   WrongIrrelevanceInLambda{}               -> "WrongIrrelevanceInLambda"
   WrongQuantityInLambda{}                  -> "WrongQuantityInLambda"
   WrongCohesionInLambda{}                  -> "WrongCohesionInLambda"
+  WrongPolarityInLambda{}                  -> "WrongPolarityInLambda"
   WrongNamedArgument{}                     -> "WrongNamedArgument"
   WrongNumberOfConstructorArguments{}      -> "WrongNumberOfConstructorArguments"
   QuantityMismatch{}                       -> "QuantityMismatch"
@@ -364,6 +365,9 @@ instance PrettyTCM TypeError where
 
     WrongCohesionInLambda ->
       fwords "Incorrect cohesion annotation in lambda"
+
+    WrongPolarityInLambda ->
+      fwords "Incorrect polarity annotation in lambda"
 
     WrongNamedArgument a xs0 -> fsep $
       pwords "Function does not accept argument "
